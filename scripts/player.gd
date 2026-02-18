a67627300b8b2e6f96308f2b194c96ccb2e8fa5e
Allison Johanson <allymjohanson@gmail.com>
2026-02-18 15:50:33 -0700

    Finished tutorial

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed = 700
 @export var gravity = 30
-@export var jump_force = 300
+@export var jump_force = 900
+@export var max_fall = 2000
 
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y += gravity
-		if velocity.y > 500:
-			velocity.y = 500
+		if velocity.y > max_fall:
+			velocity.y = max_fall
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump"):# &&  is_on_floor():
 		velocity.y = -jump_force
-	
+	 
 	var horizonal_direction = Input.get_axis("move_left","move_right" )
 	velocity.x = speed  * horizonal_direction
 	move_and_slide()
