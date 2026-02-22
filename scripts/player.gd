438b12f27eadd20bdc65288f6c43fee0df1216f1
Allison Johanson <allymjohanson@gmail.com>
2026-02-22 01:59:07 -0700

    Initialized animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var speed = 700
 @export var gravity = 30
 @export var jump_force = 900
 @export var max_fall = 2000
+
+func _ready():
+	animated_sprite_2d.stop()
+	animated_sprite_2d.play("idle")
 
 func _physics_process(_delta: float) -> void:
 	if !is_on_floor():
@@ -17,4 +22,18 @@ func _physics_process(_delta: float) -> void:
 	var horizonal_direction = Input.get_axis("move_left","move_right" )
 	velocity.x = speed  * horizonal_direction
 	move_and_slide()
-	pass
+	if velocity.y < 0:
+		animated_sprite_2d.play("fall")
+		animated_sprite_2d.flip_h = true
+	elif velocity.y > 0:
+		animated_sprite_2d.play("jump")
+		animated_sprite_2d.flip_h = false
+	elif horizonal_direction < 0:
+		animated_sprite_2d.play("walk")
+		animated_sprite_2d.flip_h = true
+	elif horizonal_direction > 0:
+		animated_sprite_2d.play("walk")
+		animated_sprite_2d.flip_h = false
+	else:
+		animated_sprite_2d.play("idle")
+	
