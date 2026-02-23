93a6b072c8a73f15130333f91355bdd8333160bd
Allison Johanson <allymjohanson@gmail.com>
2026-02-22 19:29:00 -0700

    new level ganeration

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@export var speed = 700
+@export var speed = 500
 @export var gravity = 30
 @export var jump_force = 900
 @export var max_fall = 2000
@@ -22,18 +22,12 @@ func _physics_process(_delta: float) -> void:
 	var horizonal_direction = Input.get_axis("move_left","move_right" )
 	velocity.x = speed  * horizonal_direction
 	move_and_slide()
-	if velocity.y < 0:
+	if velocity.y > 0:
 		animated_sprite_2d.play("fall")
-		animated_sprite_2d.flip_h = true
-	elif velocity.y > 0:
+	elif velocity.y < 0:
 		animated_sprite_2d.play("jump")
-		animated_sprite_2d.flip_h = false
-	elif horizonal_direction < 0:
+	elif horizonal_direction != 0:
 		animated_sprite_2d.play("walk")
-		animated_sprite_2d.flip_h = true
-	elif horizonal_direction > 0:
-		animated_sprite_2d.play("walk")
-		animated_sprite_2d.flip_h = false
 	else:
 		animated_sprite_2d.play("idle")
 	
--- scripts/tower.gd
+++ scripts/tower.gd
@@ -2,21 +2,22 @@ extends Node2D
 var rng: RandomNumberGenerator
 
 # Objects
-var block = preload("res://scenes/platform.tscn")
-var platform = preload("res://scenes/half_platform.tscn")
+var block = preload("res://scenes/block.tscn")
+var platform = preload("res://scenes/platform.tscn")
 #Textures
 var grass_texture = preload("res://assets/sprites/tiles/grass_dirt.png")
+var dirt_texture = preload("res://assets/sprites/tiles/dirt_block.png")
 var wall_texture = preload("res://assets/sprites/tiles/stone_wall.png")
-var platform_texture = preload("res://assets/sprites/tiles/dirt_block.png")
-var gift_platform_texture = preload("res://assets/sprites/tiles/dirt_block.png")
+var platform_texture = preload("res://assets/sprites/tiles/path_stone_slab.png")
+var gift_platform_texture = preload("res://assets/sprites/tiles/snow_stone_slab.png")
 
 # Important Numbers
 @export var level_width = 14
-@export var floors = 10
-@export var block_size = 64
+@export var floors = 25
+@export var block_size = 64.0
 @export var platform_chance = 0.2
-var wall_pos = (level_width/2)*block_size
-var total_platforms = floors*5
+var wall_pos = (level_width*block_size)/2
+var total_platforms = floors
 
 func spawn_object(x,y,object,texture=null):
 	var p = object.instantiate()
@@ -32,19 +33,33 @@ func get_unique_random_numbers(min_val: int, max_val: int, count: int) -> Array:
 	possible_numbers.shuffle()
 	return possible_numbers.slice(0, count)
 
-func build_floors(y_pos, index_array):
-#	for each of the slots
-
-	pass
+func build_platform(y_pos, is_gift):
+	var x_pos = randf_range(-wall_pos+block_size, wall_pos-block_size)
+	if is_gift != -1:
+		spawn_object(x_pos,y_pos, platform, gift_platform_texture)
+	else:
+		spawn_object(x_pos,y_pos, platform, platform_texture)
 
 func build_walls():
-	pass
+	var y_pos = block_size
+	for j in range(-5,level_width+6):
+		spawn_object((-wall_pos+j*block_size),y_pos, block, grass_texture)
+		spawn_object((-wall_pos+j*block_size),y_pos+block_size, block, dirt_texture)
+	for i in range(floors+4):
+		#if i == 0:
+		y_pos -= block_size
+		spawn_object(-wall_pos,y_pos, block, wall_texture)
+		spawn_object(wall_pos,y_pos, block, wall_texture)
+	for j in range(1,level_width-1):
+		spawn_object((-wall_pos+j*block_size),y_pos, platform, platform_texture)
 
 func build_tower():
-	var y_pos = block_size
-	var gift_legs = get_unique_random_numbers(1,total_platforms,5)
-
+	var y_pos = -block_size*2
+	var gift_platforms = get_unique_random_numbers(0,total_platforms-1,5)
+	for i in range(total_platforms):
+		build_platform(y_pos-i*block_size, gift_platforms.find(i))
 
 func _ready():
 	rng = RandomNumberGenerator.new()
+	build_walls()
 	build_tower()
